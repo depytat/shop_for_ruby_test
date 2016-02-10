24ff30b8133e0ff76556e19222926c0f8ac3b986
d.zyabko <d.zyabko@itransition.com>
2016-02-10 20:08:28 +0300

    hash

---
+++ hash.rb
@@ -0,0 +1,6 @@
+my_data = {:name => "James", :family => "Bond"}
+
+puts my_data[:name]
+puts my_data[:family]
+
+p my_data
--- item.rb
+++ item.rb
@@ -1,16 +1,39 @@
 class Item
-  def price
-    @price
-  end 
+ 
+#------метод инитиолайз без хеша-------
+# def initialize
+#   @price = 30
+#   @weight = 150
+# end  
 
-  def price=(price_value)
-    @price = price_value
-  end  
+# ---------геттеры и сеттеры---------
+#  def price
+#    @price
+# end
+# def price=(price_value)
+#   @price = price_value
+# end
+
+#--------ридеры и райтеры--------------------
+# attr_reader :price, :weight
+# attr_writer :price, :weight
+
+def initialize(options)
+  @price = options[:price]
+  @weight = options[:weight]
 end
 
-item1 = Item.new
-item1.price=(10)
-    
+  attr_accessor :price, :weight
 
+end
+
+# item1 = Item.new простое создание элемента
+item1 = Item.new({ :price => 30, :weight => 250})  # создание элемента с начальными параметрами в хеше
 puts item1.price
+puts item1.weight
+
+# item1.price=(10)   задание сеттером значения
+# item1.weight=(300)  
+puts item1.price
+puts item1.weight
 
